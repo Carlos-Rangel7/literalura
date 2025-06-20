5746bd26d6af2e0afbe4477892a1c29092607d50
Carlos Rangel <croi7726@gmail.com>
2025-06-20 20:37:21 -0300

    feat: cria entidades Autor e Livro e DTOs correspondentes

---
+++ src/main/java/com/literalura/model/Autor.java
@@ -0,0 +1,57 @@
+package com.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeAutor;
+    private Integer anoNascimento;
+    private Integer anoFalecimento;
+
+    @ManyToMany(mappedBy = "autores", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Livro> livros = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public Integer getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(Integer anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public Integer getAnoFalecimento() {
+        return anoFalecimento;
+    }
+
+    public void setAnoFalecimento(Integer anoFalecimento) {
+        this.anoFalecimento = anoFalecimento;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+}
---
+++ src/main/java/com/literalura/model/Livro.java
@@ -0,0 +1,56 @@
+package com.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String idioma;
+    private int numeroDeDownloads;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Autor> autores;
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public int getNumeroDeDownloads() {
+        return numeroDeDownloads;
+    }
+
+    public void setNumeroDeDownloads(int numeroDeDownloads) {
+        this.numeroDeDownloads = numeroDeDownloads;
+    }
+}
